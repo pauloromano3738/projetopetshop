1c1d8cf092d13a45326a6dfaf2226062e4198e78
pauloromano3738 <pauloromano3738@gmail.com>
2024-05-07 17:50:34 -0300

    adicionando comentários

--- login.py
+++ login.py
@@ -21,12 +21,12 @@ with st.form(key="login"): #usando a notação with
 
 if botao_login: #se botao_login for clicado execute:
 
-    #a váriavel valido recebe um booleano 
-    valido = loginController.verificar_login(profissional.Profissional(None, None, None, None, login, senha))
+    #a váriavel valido recebe um booleano retornado da função verificar_login de loginController 
+    valido = loginController.verificar_login(profissional.Profissional(None, None, None, None, login, senha)) #a função verificar_login recebe como parâmetro a instância de um objeto profissional e passa os parâmetros login e senha
 
-    if valido:
-        st.success("Login realizado com sucesso!")
-        st.switch_page("pages/home.py")
-    else:
-        st.error("Login incorreto. Tente novamente.")
+    if valido: #se o valor da váriavel for True execute:
+        st.success("Login realizado com sucesso!") #a função success mostra uma mensagem de sucesso na tela
+        st.switch_page("pages/home.py") #a função switch_page redireciona o sistema para uma página que é passada no parâmetro, que no caso é a pasta pages e o arquivo home.py
+    else: #se não
+        st.error("Login incorreto. Tente novamente.") #a função error recebe uma string como parâmetro e mostra como mensagem de error na tela
 
